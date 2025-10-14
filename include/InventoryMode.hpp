e60b3dbf2a9503cf84d0dc0ea4961eefdc15f643
stream-enterer <arotondo@g.ucla.edu>
2025-10-13 21:40:50 -0700

    add missing file

---
+++ include/InventoryMode.hpp
@@ -0,0 +1,13 @@
+#ifndef INVENTORY_MODE_HPP
+#define INVENTORY_MODE_HPP
+
+namespace tutorial
+{
+    enum class InventoryMode
+    {
+        Use,
+        Drop
+    };
+} // namespace tutorial
+
+#endif // INVENTORY_MODE_HPP
